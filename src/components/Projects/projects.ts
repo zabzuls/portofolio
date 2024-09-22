a9a2f8d7b55bed3ccf58442c8b355598163062b7
Zabzul Supratman <zabzuls@gmail.com>
2024-09-22 13:01:46 +0700

    [feat] add card

---
+++ src/components/Projects/projects.ts
@@ -0,0 +1,25 @@
+type Project = {
+    namaProject: string;
+    posisi: string;
+    keterangan: string;
+  };
+  const projects: Project[] = [
+    {
+      namaProject: "Pembangunan Website IntraDIKT",
+      posisi: "Front-End Developer",
+      keterangan: "Mengerjakan fitur data gaji PPNPN dan import/export data pegawai"
+    },
+    {
+      namaProject: "Website Portofolio",
+      posisi: "Full-Stack Developer",
+      keterangan: "Membuat website portofolio menggunakan Next.js dan Tailwind CSS"
+    },
+    {
+      namaProject: "Website Portofolio",
+      posisi: "Full-Stack Developer",
+      keterangan: "Membuat website portofolio menggunakan Next.js dan Tailwind CSS"
+    }
+  ];
+  
+  export default projects;
+  
